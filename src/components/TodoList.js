64b9a760bb61259c136fb23422ef4bffe2d77996
LEEHOJIN <cacabi24@hanmail.net>
2023-01-26 16:46:05 +0900

    ✔ TodoItem 컴포넌트 추가
    ✔ TodoItem 컴포넌트 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -9,7 +10,14 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="TEMP_1" done={true} />
+      <TodoItem text="TEMP_2" done={true} />
+      <TodoItem text="TEMP_3" done={false} />
+      <TodoItem text="TEMP_4" done={false} />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
