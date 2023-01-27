0964f70aeb91ba0ec2e78b3e76a15fce14808f3f
LEEHOJIN <cacabi24@hanmail.net>
2023-01-27 10:02:09 +0900

    ✔ localStorage 이용
    ✔ localStorage 이용

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -1,16 +1,41 @@
 import React, { useReducer, createContext, useContext, useRef } from 'react';
 
 const initialTodos = [];
+if(JSON.parse(localStorage.getItem('todo'))){
+  let localStore = JSON.parse(localStorage.getItem('todo'));
+  initialTodos.push(...localStore);
+}
+
+function convertToJson(state, action) {
+  if(action.type === "CREATE") {
+    return JSON.stringify(state.concat(action.todo));
+  };
+  if(action.type === "TOGGLE") {
+    return JSON.stringify(
+      state.map(todo =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+  if(action.type === "REMOVE") {
+    return JSON.stringify(
+      state.filter(todo => todo.id !== action.id)
+    );
+  };
+}
 
 function todoReducer(state, action) {
   switch (action.type) {
     case 'CREATE':
+      localStorage.setItem('todo', convertToJson(state, action));
       return state.concat(action.todo);
     case 'TOGGLE':
+      localStorage.setItem('todo', convertToJson(state, action));
       return state.map(todo =>
         todo.id === action.id ? { ...todo, done: !todo.done } : todo
       );
     case 'REMOVE':
+      localStorage.setItem('todo', convertToJson(state, action));
       return state.filter(todo => todo.id !== action.id);
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
@@ -23,7 +48,7 @@ const TodoNextIdContext = createContext();
 
 export function TodoProvider({ children }) {
   const [state, dispatch] = useReducer(todoReducer, initialTodos);
-  const nextId = useRef(5);
+  const nextId = useRef(1);
 
   return (
     <TodoStateContext.Provider value={state}>
